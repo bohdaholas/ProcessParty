4137e5f83f34916c73e8ac322aad290dbe3f9236
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-12-29 16:18:55 +0200

    finish writing tests for file mapping

--- tests/interprocess/test_file_mapping.cpp
+++ tests/interprocess/test_file_mapping.cpp
@@ -49,6 +49,36 @@ TEST_F(FileMappingFixture, TestSingleArgConstructor) {
     ASSERT_EQ(read_write, fm.get_mode());
 }
 
+TEST_F(FileMappingFixture, Test2ArgConstructor) {
+    ASSERT_EQ(true, file_exists(filename));
+    ppip::file_mapping fm(filename, read_only);
+    ASSERT_EQ(filename, fm.get_name());
+    ASSERT_EQ(read_only, fm.get_mode());
+}
+
+TEST_F(FileMappingFixture, Test3ArgConstructor) {
+    ASSERT_EQ(true, file_exists(filename));
+    ASSERT_NO_THROW(ppip::file_mapping fm(filename,
+                                          read_only,
+                            open_or_create));
+}
+
+TEST_F(FileMappingFixture, OpenOnlyWhenNotExist) {
+    ASSERT_EQ(true, file_exists(filename));
+    ppip::file_mapping::remove(filename);
+    ASSERT_EQ(false, file_exists(filename));
+    ASSERT_THROW(ppip::file_mapping fm(filename,
+                                          read_only,
+                            open_only), runtime_error);
+}
+
+TEST_F(FileMappingFixture, CreateOnlyWhenExist) {
+    ASSERT_EQ(true, file_exists(filename));
+    ASSERT_THROW(ppip::file_mapping fm(filename,
+                                       read_only,
+                         create_only), runtime_error);
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
